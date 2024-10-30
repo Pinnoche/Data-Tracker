c2836c6539388c4c23dc91ff72dd22cab1615704
Pinnoche <ptime552@gmail.com>
2024-10-30 01:42:36 +0100

    feeder_11

--- src/components/FeedersBase.jsx
+++ src/components/FeedersBase.jsx
@@ -1,38 +1,179 @@
-
-
-function FeedersBase({Feeder_33, trasmission_1, trasmission_2, trasmission_3, trasmission_4 }) {
+function FeedersBase({
+  Feeder_33,
+  trasmission_1,
+  trasmission_2,
+  trasmission_3,
+  trasmission_4,
+  F1,
+  F2,
+  F3,
+  F4,
+  F5,
+}) {
   return (
     <div className="w-full">
-          <div className="w-full flex flex-col items-center">
-            <div className="bg-white max-w-xs px-4 py-2 rounded-md uppercase border text-sm text-center drop-shadow-md">
-              {Feeder_33}
-            </div>
-          </div>
-          <div className="h-12 max-w-2 border-l border-black mx-auto"></div>
-          <div className="w-[92%] border-t border-black mx-auto"></div>
-          <div className="flex justify-between max-w-[92%] mx-auto">
-            <div className="h-12 border-l border-black"></div>
-            <div className="h-12 border-l border-black"></div>
-            <div className="h-12 border-l border-black"></div>
-            <div className="h-12 border-l border-black"></div>
-          </div>
+      <div className="w-full flex justify-center">
+        <div className="bg-white max-w-xs px-6 py-2 rounded-md uppercase border text-sm text-center drop-shadow-md">
+          {Feeder_33}
+        </div>
+      </div>
+      <div className="h-12 max-w-2 border-l border-black mx-auto"></div>
 
-          <div className="flex justify-between">
-            <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-4">
-              {trasmission_1}
+      <div className="w-[88%] border-t border-black mx-auto"></div>
+
+      <div className="flex justify-between max-w-[88%] mx-auto">
+        <div className="h-12 border-l border-black"></div>
+        <div className="h-12 border-l border-black"></div>
+        <div className="h-12 border-l border-black"></div>
+        <div className="h-12 border-l border-black"></div>
+      </div>
+
+      <div className="flex justify-between">
+        <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-12">
+          {trasmission_1}
+        </div>
+        <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-12">
+          {trasmission_2}
+        </div>
+        <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-12">
+          {trasmission_3}
+        </div>
+        <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-12">
+          {trasmission_4}
+        </div>
+      </div>
+
+      <div className="h-72 flex justify-between">
+        <div className="h-full flex">
+          <div className=" h-full pt-20 pb-6 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F2}
             </div>
-            <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-4">
-              {trasmission_2}
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F4}
             </div>
-            <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-4">
-              {trasmission_3}
+          </div>
+          <div className="h-full pb-12 pt-24 flex flex-col justify-between">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="max-w-2 border-l border-black"></div>
+          <div className="h-full flex flex-col justify-between pt-10">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="h-full pt-3 mt-4 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F1}
             </div>
-            <div className="bg-gray-100 max-w-xs uppercase font-normal text-xs drop-shadow-lg text-center py-3 px-4">
-              {trasmission_4}
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F3}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F5}
             </div>
           </div>
         </div>
-  )
+
+        <div className="h-full flex">
+          <div className=" h-full pt-20 pb-6 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F2}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F4}
+            </div>
+          </div>
+          <div className="h-full pb-12 pt-24 flex flex-col justify-between">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="max-w-2 border-l border-black"></div>
+          <div className="h-full flex flex-col justify-between pt-10">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="h-full pt-3 mt-4 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F1}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F3}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F5}
+            </div>
+          </div>
+        </div>
+
+        <div className="h-full flex">
+          <div className=" h-full pt-20 pb-6 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F2}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F4}
+            </div>
+          </div>
+          <div className="h-full pb-12 pt-24 flex flex-col justify-between">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="max-w-2 border-l border-black"></div>
+          <div className="h-full flex flex-col justify-between pt-10">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="h-full pt-3 mt-4 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F1}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F3}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F5}
+            </div>
+          </div>
+        </div>
+
+        <div className="h-full flex">
+          <div className=" h-full pt-20 pb-6 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F2}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F4}
+            </div>
+          </div>
+          <div className="h-full pb-12 pt-24 flex flex-col justify-between">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="max-w-2 border-l border-black"></div>
+          <div className="h-full flex flex-col justify-between pt-10">
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+            <div className="w-8 border-b border-black"></div>
+          </div>
+          <div className="h-full pt-3 mt-4 flex flex-col justify-between">
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F1}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F3}
+            </div>
+            <div className="px-4 py-2 bg-green-400 rounded-md max-w-16">
+              {F5}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default FeedersBase
+export default FeedersBase;
--- src/pages/disco/Sub2.jsx
+++ src/pages/disco/Sub2.jsx
@@ -3,9 +3,9 @@ function Sub2() {
   return (
     <div className="h-full w-full bg-white text-black p-6">
       <div className="container flex flex-col items-center gap-y-16">
-        <FeedersBase Feeder_33="Oluyole" trasmission_1="trasmission_1" trasmission_2="trasmission_2" trasmission_3="trasmission_3" trasmission_4="trasmission_4" />
-        <FeedersBase Feeder_33="Oluyole" trasmission_1="trasmission_1" trasmission_2="trasmission_2" trasmission_3="trasmission_3" trasmission_4="trasmission_4" />
-        <FeedersBase Feeder_33="Osogbo" trasmission_1="KJ-123" trasmission_2="hfsk-33MGH" trasmission_3="highMF" trasmission_4="hfsdhhgsf" />
+        <FeedersBase Feeder_33="Oluyole" trasmission_1="trasmission_1" trasmission_2="trasmission_2" trasmission_3="trasmission_3" trasmission_4="trasmission_4" F1="F1" F2="F2" F3="3" F4="F4" F5="F5" />
+        <FeedersBase Feeder_33="Oluyole" trasmission_1="trasmission_1" trasmission_2="trasmission_2" trasmission_3="trasmission_3" trasmission_4="trasmission_4" F1="F1" F2="F2" F3="3" F4="F4" F5="F5" />
+        <FeedersBase Feeder_33="Osogbo" trasmission_1="KJ-123" trasmission_2="hfsk-33MGH" trasmission_3="highMF" trasmission_4="hfsdhhgsf" F1="F1" F2="F2" F3="3" F4="F4" F5="F5" />
         
       </div>
     </div>
